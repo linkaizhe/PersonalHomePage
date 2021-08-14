5a043c366ecab52742578cc5b61023465588577b
linkaizhe <499780002@qq.com>
2021-08-15 00:19:49 +0800

    网页底部

--- js/index.js
+++ js/index.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
 
 			<a href="https://www.baidu.com/" class="plyr__control" aria-label="Playlist">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M15,13H9a1,1,0,0,0,0,2h6a1,1,0,0,0,0-2Zm0-4H9a1,1,0,0,0,0,2h6a1,1,0,0,0,0-2ZM12,2A10,10,0,1,0,22,12,10,10,0,0,0,12,2Zm0,18a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"/></svg>
-				<span class="plyr__tooltip" role="tooltip">歌单</span>
+				<span class="plyr__tooltip" role="tooltip">播放列表</span>
 			</a>
 		</div>
 	</div>
